b887dc5862ce1b299cfe2e1ddfee20108e931839
AlirezaNaseri <naseri.alireza.baziari@gmail.com>
2024-03-04 22:29:55 +0330

    send two model to Home/Index

---
+++ resume.Application/DTO/Home-Index/MyskillsandEducation.cs
@@ -0,0 +1,16 @@
+﻿using resume.Domain.Entities.Education;
+using resume.Domain.Entities.MySkills;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace resume.Application.DTO.Home_Index
+{
+	public class MyskillsandEducation
+	{
+        public List<Education> educationslist { get; set; }
+        public List<MySkills> myskillslist { get; set; }
+    }
+}
